49e80309b3770be8bdc1bb83c502b5156ee6cfff
Venonh2 <brendow-evan@hotmail.com>
2020-07-18 16:35:10 -0300

    salvando backend e frontend da aplicação

---
+++ backend/src/routes.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+//import knex from './database/connection'
+
+import PointsController from './controllers/pointsController'
+import ItemsController from './controllers/itemsController'
+
+const routes = express.Router()
+
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create)
+
+routes.get('/points/:id', pointsController.show)
+
+routes.get('/points', pointsController.index)
+
+//index, show, create, update, delete
+
+export default routes;
